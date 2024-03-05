240d6243b16612d4d8687c8da728e170423c80ad
joho2022 <104732020+joho2022@users.noreply.github.com>
2024-03-05 12:29:48 +0900

    KAI - IBOutlet 연결하기 (#3)
    * feat: TabBar 컨트롤러 추가 및 초기 뷰 컨트롤러로 설정
    
    * no message
    
    * README 업데이트: IBOutlet 연결하기, UILabel 클래스 속성 내용 추가
    
    * README 수정: IBOutlet내용의 결과 사진 크기 수정
    
    * feat: IBOutlet 연결
    
    * feat: IBOutlet 연결

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -8,14 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet var firstDescription: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(#file, #line, #function, #column)
+        
+        
+        self.firstLabel.text = "KAI의 사진액자"
+        self.firstDescription.text = "KAI의 사진액자 설명입니다."
+        
+        
+        let fullText = firstLabel.text ?? ""
+        let attributedString = NSMutableAttributedString(string: fullText)
+        
+        let range1 = (fullText as NSString).range(of: "KAI")
+        
+        attributedString.addAttribute(.foregroundColor, value: UIColor.blue, range: range1)
+        
+        let range2 = (fullText as NSString).range(of: "액자")
+        
+        attributedString.addAttribute(.foregroundColor, value: UIColor.blue, range: range2)
+        
+        firstLabel.attributedText = attributedString
+        
     }
-
-
-   
+    
+    
+    
 }
 
